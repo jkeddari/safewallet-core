a0b90d36257c7553bdcc8050d5c9ca72599c6cf1
Jordan KEDDARI <jordan@Jordans-MacBook-Pro.local>
2023-02-05 22:00:53 +0100

    add eslint/prettier

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
 require('dotenv').config()
 
 console.log(process.env.MUMBAI_RPC_URL as string)
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: '0.8.17',
   paths: {
-    artifacts: './src/artifacts',
+    artifacts: './artifacts',
   },
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 1337,
     },
     mumbai: {
       url: process.env.MUMBAI_RPC_URL as string,
@@ -24,11 +24,11 @@ const config: HardhatUserConfig = {
     goerli: {
       url: process.env.GOERLI_RPC_URL as string,
       accounts: [process.env.PRIVATE_KEY as string],
-    }
+    },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
-  }
-};
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+}
 
-export default config;
+export default config
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,19 +1,19 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat'
 
 async function main() {
-  const [owner, addr1] = await ethers.getSigners();
-  const Wallet = await ethers.getContractFactory("SafeWallet");
+  const [owner, addr1] = await ethers.getSigners()
+  const Wallet = await ethers.getContractFactory('SafeWallet')
   let owners: string[] = [owner.address, addr1.address]
-  const wallet = await Wallet.deploy(owners, 1);
+  const wallet = await Wallet.deploy(owners, 1)
 
-  await wallet.deployed();
+  await wallet.deployed()
 
-  console.log(`SafeWallet deployed to ${wallet.address}`);
+  console.log(`SafeWallet deployed to ${wallet.address}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
